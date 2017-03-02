ec86701a74dac1b058a5d4cf6de6d3d143e53853
Stephen <stephen.chan9@yahoo.com>
2017-03-01 20:42:24 -0800

    Added changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
+
 
 function setDetails(imageURL, titleText) {
     'use strict';
@@ -31,6 +36,7 @@ function addThumbClickHandler(thumb) {
     thumb.addEventListener('click', function(event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     });
 }
 
@@ -41,10 +47,46 @@ function getThumbnailsArray() {
     return thumbnailArray;
 }
 
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+
+    setTimeout(function() {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+//49-53 are keys 1-5
+function addKeyPressHandler() {
+    var thumbnails = getThumbnailsArray();
+    'use strict';
+    document.body.addEventListener('keyup', function(event) {
+        event.preventDefault();
+        //console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+        if (event.keyCode >= 49 && event.keyCode <= 58) {
+            setDetailsFromThumb(thumbnails[event.keyCode - 49]);
+        }
+        //setDetailsFromThumb(thumb);
+    });
+}
+
+
+
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler(thumbnails);
 }
 
 initializeEvents();
